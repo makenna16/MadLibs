f69704abcebdcc88e1f31d636efe3be4829100c5
makenna <makenna.gordon.2023@student.lhschools.org>
2020-03-04 13:28:31 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,95 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Phase 1
+        String food;
+        String favFood;
+        String favColor;
+        String favAmusementPark;
+        String favShoeBrand;
+        String bandYouLike;
+        String favSong;
+        String favMovieGenre;
+        String favShow;
+        String foodYouLike;
+        String favCelebrity;
+        String placeYouWantToVisit;
+        String favVideoGame;
+        String favClothingBrand;
+        String favSchoolSubject;
+        String favWord;
+        String favCandy;
+        String favAppOnYourPhone;
+        String favDate;
+        String favTime;
+
+        //phase 2
+
+
+
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("What is your fav video game");
+        favVideoGame = keyboard.nextLine();
+        System.out.println("What is your fav word");
+        favWord = keyboard.nextLine();
+        System.out.println("What is your fav time");
+        favTime = keyboard.nextLine();
+        System.out.println("What is fav school subject");
+        favSchoolSubject = keyboard.nextLine();
+        System.out.println("What is your fav clothing brand");
+        favClothingBrand = keyboard.nextLine();
+        System.out.println("What is a place you want to visit");
+        placeYouWantToVisit = keyboard.nextLine();
+        System.out.println("What is your fav celebrity");
+        favCelebrity = keyboard.nextLine();
+        System.out.println("What's your fav app on your phone");
+        favAppOnYourPhone = keyboard.nextLine();
+        System.out.println("What's your favortite date");
+        favDate = keyboard.nextLine();
+        System.out.println("What is a food you like");
+        favFood = keyboard.nextLine();
+        System.out.println("What's your favorite show");
+        favShow = keyboard.nextLine();
+        System.out.println("Favorite amusement park");
+        favAmusementPark = keyboard.nextLine();
+        System.out.println("Band you like");
+        bandYouLike = keyboard.nextLine();
+        System.out.println("food you like (not your favorite)");
+        food = keyboard.nextLine();
+        System.out.println("What's your fav shoe brand");
+        favShoeBrand = keyboard.nextLine();
+        System.out.println("whats your favorite candy");
+        favCandy = keyboard.nextLine();
+
+
+        //phase 3
+
+        System.out.println("Once upon a time in a land called "+ favVideoGame + ",");
+        System.out.println("There was a person named " + favWord + ".");
+        System.out.println("Who was " + favTime+ " years old,");
+        System.out.println("" + favWord +" was obsessed with " + favSchoolSubject+ ".");
+        System.out.println(""+ favWord + " favorite food was " + favClothingBrand + ".");
+        System.out.println("" +favWord + "Had a sibling named," + placeYouWantToVisit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
